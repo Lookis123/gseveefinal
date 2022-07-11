815b151ff5d3382419acc3ac40d2c1e2ef39df96
Dimitris Boulasikis <boulasikisdimitris@gmail.com>
2022-07-12 02:36:43 +0300

    Update staticData.js

--- src/utils/staticData.js
+++ src/utils/staticData.js
@@ -877,7 +877,7 @@ export const category_menus = [
     items: [
       {
         name: "Διεθνείς σχέσεις",
-        to: "/category/international-relation",
+        to: "/dash_Institutional",
       },
       {
         name: "Κοινωνικός διάλογος",
